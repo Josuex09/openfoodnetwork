0467201916e243f7db64615722479700087f9dde
josue <josuex09@hotmail.es>
2016-09-24 17:47:45 -0600

    modal tab

--- app/controllers/stadistics_controller.rb
+++ app/controllers/stadistics_controller.rb
@@ -1,35 +1,43 @@
-require "json"
 class StadisticsController < BaseController
   layout 'stadistics'
   
-  $product_types = ["Vegetables","Fruit","Oils","Preserves and Sauces","Dairy","Meat and Fish"];
-  $hash = Hash[$product_types.map.with_index.to_a];
   $graphics = [];
   $id = 0;
 
   def index
      @graphics = $graphics 
-   
   end
 
 
-  def create_chart   
+  def create_product_chart(params)
     product_type = params[:product_type]
     chart_type = params[:chart_type]
     initial_date = params[:initial_date]
     final_date = params[:final_date]
     
     pgraphic = ProductGraphic.new(initial_date,final_date,chart_type,$id)
-    pgraphic.generate 
+    pgraphic.generate
+    return pgraphic    
+  end
+
+  def create_chart  
+    tab = params[:modal_tab];
+    
+    puts "#{tab}"
+    
+    if tab == "1"
+      graphic = create_product_chart(params)
+    else
+      redirect_to :action => "index"
+    end  
     $id +=1
     
-    if(!pgraphic.includes?($graphics))
-      $graphics.push(pgraphic)
+    if(!graphic.includes?($graphics))
+      $graphics.push(graphic)
     end
         
     redirect_to :action =>  "index"
   
   end
- 
-    
+   
 end
