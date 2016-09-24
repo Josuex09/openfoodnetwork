d3176ce4359f101e6da4a6a27baf5454ddf380b5
josue <josuex09@hotmail.es>
2016-09-24 02:44:05 -0600

    first product grahic done

--- app/controllers/stadistics_controller.rb
+++ app/controllers/stadistics_controller.rb
@@ -1,18 +1,16 @@
 require "json"
 class StadisticsController < BaseController
   layout 'stadistics'
-  before_filter :init
   
   $product_types = ["Vegetables","Fruit","Oils","Preserves and Sauces","Dairy","Meat and Fish"];
   $hash = Hash[$product_types.map.with_index.to_a];
   $graphics = [];
+  $id = 0;
 
   def index
-  end
-  
-  def init
     @graphics = $graphics
   end
+  
     
   def create_chart   
     product_type = params[:product_type]
@@ -20,52 +18,18 @@ class StadisticsController < BaseController
     initial_date = params[:initial_date]
     final_date = params[:final_date]
     
-    #Arreglo con valor de cada tipo
-    values = [];
-    for i in 0..$product_types.length-1
-      values.push(0);
-    end
+    pgraphic = ProductGraphic.new(initial_date,final_date,chart_type,$id)
+    pgraphic.generate 
+    $id +=1
     
-    @time = "Product types sold since"
-    #Este codigo, da las categorias de los productos que se vendieron
-    if(final_date == "" && initial_date == "")
-      lineitems = Spree::LineItem.all;
-      @time += " always";
-    else
-      lineitems = Spree::LineItem.where("created_at >= :start_date AND created_at <= :end_date",{start_date: initial_date, end_date: final_date})
-      @time += " "+initial_date.to_s+" untill "+final_date.to_s;
+    if(!pgraphic.includes?($graphics))
+      $graphics.push(pgraphic)
     end
-    
-    lineitems.each do |item|
-      variant_id = item.variant_id;
-      variant = Spree::Variant.find(variant_id);
-      product_id = variant.product_id;
-      product = Spree::Product.find(product_id);
-      taxon_id = product.taxon_ids[0];
-      taxon = Spree::Taxon.find(taxon_id);
-      values[$hash[taxon.name]] +=1;   
-    end
-    new_chart = hash_to_json(values,$product_types);
-    # Validar si ya existe
-    if(!$graphics.include? new_chart)
-      $graphics.push(new_chart);
-    end
-    render "index"
+        
+    #render "index"
+    redirect_to :action =>  "index"
   
   end
-  
-  
-  
-  def hash_to_json(values,arr)
-    result = '['
-    for i in 0..arr.length-1
-      arr[i] = arr[i][0...7];
-      result += '{ "type" : "'+arr[i]+'" , "value": '+values[i].to_s+'},';
-    end
-    result = result[0...result.length-1];
-    result += "]";
-    return result
-  end
-   
+ 
     
 end
